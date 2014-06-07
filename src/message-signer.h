fc65c8617f732b5aba4b867e26ce6eadcd8a70db
zbierak <zbierak@gmail.com>
2014-06-07 15:44:58 +0200

    Function for getting unique message signer identifiers

--- src/message-signer.h
+++ src/message-signer.h
@@ -9,6 +9,7 @@
 #define MESSAGE_SIGNER_H_
 
 #include "topology/topology.h"
+#include <inttypes.h>
 
 namespace bento {
 
@@ -19,6 +20,8 @@ public:
 
 	virtual void signMessage(const Topology& topology, const std::string& to, const int32_t type, const std::string& msg, std::string& signature) = 0;
 	virtual bool verifyMessage(const Topology& topology, const std::string& from, const int32_t type, const std::string& msg, const std::string& signature) = 0;
+
+	inline virtual int32_t getUniqueSignerId() { return -1; }
 };
 
 } /* namespace bento */
